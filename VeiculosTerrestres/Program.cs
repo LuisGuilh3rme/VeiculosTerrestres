b816c9797c8365b777d7ed27574178a3fb777042
Luis Guilherme <luislebert01@outlook.com>
2023-03-14 14:30:41 -0300

    Add project files.

---
+++ VeiculosTerrestres/Program.cs
@@ -0,0 +1,14 @@
+﻿using VeiculosTerrestres;
+
+VeiculoTerrestre carro = new(4, 200.00, "Branca", "Carro");
+VeiculoTerrestre moto = new(2, 200.00, "Vermelha", "Moto");
+VeiculoTerrestre bicicleta = new(2, 40.00, "Preta", "Bicicleta");
+VeiculoTerrestre onibus = new(4, 140.00, "Azul", "Onibus");
+VeiculoTerrestre tanque = new(6, 100.00, "Verde Musgo", "Tanque");
+
+
+Console.WriteLine(carro);
+Console.WriteLine(moto);
+Console.WriteLine(bicicleta);
+Console.WriteLine(onibus);
+Console.WriteLine(tanque);
---
+++ VeiculosTerrestres/VeiculoTerrestre.cs
@@ -0,0 +1,25 @@
+﻿using System.Transactions;
+
+namespace VeiculosTerrestres
+{
+    internal class VeiculoTerrestre
+    {
+        public int Rodas { get; set; }
+        public double VelocidadeMaxima { get; set; }
+        public string Cor { get; set; }
+        public string Categoria { get; set; }
+
+        public VeiculoTerrestre(int rodas, double velocidadeMaxima, string cor, string categoria)
+        {
+            Rodas = rodas;
+            VelocidadeMaxima = velocidadeMaxima;
+            Cor = cor;
+            Categoria = categoria;
+        }
+
+        public override string ToString()
+        {
+            return $"{Categoria} {Cor}, possuindo {Rodas} rodas, velocidade máxima {VelocidadeMaxima} KM/h";
+        }
+    }
+}
